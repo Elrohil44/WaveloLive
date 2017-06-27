695c8e0dbd018dbdabfdd388e534c501ce470554
Elrohil44 <wiesiek1c@gmail.com>
2017-06-27 09:31:19 +0200

    For deployment

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -89,7 +89,7 @@ object WebServer extends JsonSupport {
     // Setting server address and port
 
     val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
-    println(s"Server online at http://localhost:8089/\nPress RETURN to stop...")
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
